093fdb85c37ad1ef054189df1cb978fec7cbae2e
victor <vickydollas11@gmail.com>
2025-08-26 13:00:04 +0100

    AC & DEL key functionality

--- index.js
+++ index.js
@@ -10,33 +10,44 @@ let operator = null
 let secondNumber = ''
 let result = ''
 let clearDisplay = false
-
-function add(a, b){
-    return a + b
+function clear(){
+    firstNumber = ''
+    operator = null
+    secondNumber = ''
+    display.textContent = '0'
 }
-function subtract(a, b){
-    return a - b
+// function add(a, b){
+//     return a + b
+// }
+// function subtract(a, b){
+//     return a - b
+// }
+// function multiply(a, b){
+//     return a * b
+// }
+// function divide(a, b){
+//     return (a === 0) ? alert('error') : a / b
+// }
+// function operate(op, num1, num2){
+//     switch (op) {
+//         case '+':
+//             return add(num1, num2)
+//         case '-':
+//             return subtract(num1, num2)
+//         case '/':
+//             return divide(num1, num2)
+//         case '*':
+//             return multiply(num1, num2)
+//         default :
+//             return null
+// }}
+function operate(op, a, b){
+    if (op === '+'){return a + b}
+    else if (op === '-'){return a - b}
+    else if (op === '*'){return a * b}
+    if (op === '/' && a !== 0){return a / b}
+    else if(a === 0){alert('error')}
 }
-function multiply(a, b){
-    return a * b
-}
-function divide(a, b){
-    return (a === 0) ? alert('error') : a / b
-}
-function operate(op, num1, num2){
-    switch (op) {
-        case '+':
-            return add(num1, num2)
-        case '-':
-            return subtract(num1, num2)
-        case '/':
-            return divide(num1, num2)
-        case '*':
-            return multiply(num1, num2)
-        default :
-            return null
-}}
-
 function appendNumber(numberString){
     if (clearDisplay){
         display.textContent = ''
@@ -68,3 +79,9 @@ operatorButton.forEach(press => {
     })
 })
 equalsButton.addEventListener('click', calculate)
+clearButton.addEventListener('click', clear)
+document.getElementById('delete').addEventListener('click', () =>{
+    if (display.textContent.length > 1){
+        display.textContent = display.textContent.slice(0, -1)
+    } else {display.textContent = '0'}
+})
